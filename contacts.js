13d3647a37be24014600153c2a2162edc1872541
AVasilaki <poseydon463@gmail.com>
2024-01-16 01:49:03 +0200

    upd

--- contacts.js
+++ contacts.js
@@ -2,25 +2,20 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
-// contacts.js
-
 const contactsPath = path.join(__dirname, "contacts.json");
 
 async function listContacts() {
-  // ...твій код. Повертає масив контактів.
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 }
 
 async function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
   const contacts = await listContacts();
   const contact = contacts.find((item) => item.id === contactId);
   return contact || null;
 }
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
   const contacts = await listContacts();
   const index = contacts.findIndex((item) => item.id === contactId);
   if (index === -1) return null;
@@ -30,7 +25,6 @@ async function removeContact(contactId) {
 }
 
 async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту (з id).
   const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
--- index.js
+++ index.js
@@ -12,29 +12,24 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const list = await contacts.listContacts(id);
       console.table(list);
-      // ...
       break;
 
     case "get":
-      // ... id
       const getBook = await contacts.getContactById(id);
       console.log(getBook);
       break;
 
     case "add":
-      // ... name email phone
       const newContact = await contacts.addContact(name, email, phone);
       console.log(newContact);
       break;
 
     case "remove":
-      // ... id
       const removeContact = await contacts.removeContact(id);
       console.log(removeContact);
       break;
@@ -44,7 +39,4 @@ async function invokeAction({ action, id, name, email, phone }) {
   }
 }
 
-// invokeAction({ action: "list" });
-// invokeAction({ action: "get", id: "qdggE76Jtbfd9eWJHrssH" });
-// invokeAction({ action: "add", name: "victoria", email: "victoria@.com", phone: "223-44-33" });
-invokeAction({ action: "remove", id: "Vme2Wl9lwgInLXFPFN4sO" });
+invokeAction(options);
